ff08e56bb6df403007a61e8aaa67da651ccd179b
jasperboyd <jasper.boyd@ten24web.com>
2016-07-17 16:17:43 -0400

    got annayang working with controlling the slides

--- js/voicecommands.js
+++ js/voicecommands.js
@@ -1,10 +1,25 @@
-if (annyang) {
-    var commands = {
-        'next': function() {
-            console.log("next"); 
-        }
-    };
+var api = impress();
+api.init();
 
-    annyang.addCommands(commands);
-    annyang.start();
+function startVoice(){
+    if (annyang) {
+        var commands = {
+            'interface next': function() {
+                console.log("you said 'next'!");
+                api.next();
+            },
+            'interface previous': function() {
+                console.log("you said 'previous'!"); 
+                var api = impress();
+                api.prev();
+            }
+            'this is awesome': function() {
+                console.log("awesome!"); 
+            }
+        };
+
+        annyang.addCommands(commands);
+        annyang.start();
+    }
 }
+
