98545e31cd1d9305974afec890bffeaa14a7030f
jasperboyd <jasper.boyd@ten24web.com>
2016-07-18 08:37:00 -0400

    simplifying commands

--- js/voicecommands.js
+++ js/voicecommands.js
@@ -4,15 +4,15 @@ api.init();
 function startVoice(){
     if (annyang) {
         var commands = {
-            'interface next': function() {
+            'next': function() {
                 console.log("you said 'next'!");
                 api.next();
             },
-            'interface previous': function() {
+            'previous': function() {
                 console.log("you said 'previous'!"); 
                 var api = impress();
                 api.prev();
-            }
+            },
             'this is awesome': function() {
                 console.log("awesome!"); 
             }
