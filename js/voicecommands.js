6d8d5e08767ced80e8a7c919c2f25a41c36cf14a
jasperboyd <jasper.boyd@ten24web.com>
2016-07-28 08:48:10 -0400

    adding speech and some slide improvements

--- js/voicecommands.js
+++ js/voicecommands.js
@@ -4,19 +4,36 @@ api.init();
 function startVoice(){
     if (annyang) {
         annyang.debug(true);
+
+        var mute = false; 
     
         var commands = {
             'next': function() {
                 console.log("you said 'next'!");
+                if(!mute){
+                    var msg = new SpeechSynthesisUtterance('Proceeding to next slide');
+                    window.speechSynthesis.speak(msg);
+                }
                 api.next();
             },
             'previous': function() {
                 console.log("you said 'previous'!"); 
+                if(!mute){
+                    var msg = new SpeechSynthesisUtterance('Ok Going Back Now');
+                    window.speechSynthesis.speak(msg);
+                }
                 var api = impress();
                 api.prev();
             },
-            'this is awesome': function() {
-                console.log("awesome!"); 
+            'computer stop talking': function() {
+                mute = true; 
+                var msg = new SpeechSynthesisUtterance('Ok Shutting Up Now'); 
+                window.speechSynthesis.speak(msg); 
+            },
+            'computer start talking': function() {
+                mute = false; 
+                var msg = new SpeechSynthesisUtterance('Thank you for the permission'); 
+                window.speechSynthesis.speak(msg); 
             }
         };
 
